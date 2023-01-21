31fa4367ff99be250bb7cacf12ca2287b1d05e55
Rattanan16092546 <rittanan16092546@gmail.com>
2023-01-21 13:04:11 +0700

    add new level water

--- Project/Edgent_ESP8266/Edgent_ESP8266.ino
+++ Project/Edgent_ESP8266/Edgent_ESP8266.ino
@@ -17,6 +17,9 @@
 //#define USE_WEMOS_D1_MINI
 
 #include "BlynkEdgent.h"
+const int pingPin = D1;
+int inPin = D2;
+
 
 BLYNK_WRITE(V0){
   int val = param.asInt();
@@ -34,5 +37,36 @@ void setup()
 
 void loop() {
   BlynkEdgent.run();
+
+
+
+  long duration, cm;
+
+pinMode(pingPin, OUTPUT);
+
+
+digitalWrite(pingPin, LOW);
+delayMicroseconds(2);
+digitalWrite(pingPin, HIGH);
+delayMicroseconds(5);
+digitalWrite(pingPin, LOW);
+pinMode(inPin, INPUT);
+duration = pulseIn(inPin, HIGH);
+
+cm = microsecondsToCentimeters(duration);
+
+Serial.print(cm);
+Serial.print("cm");
+Serial.println();
+delay(100);
 }
 
+
+
+long microsecondsToCentimeters(long microseconds)
+{
+// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+// The ping travels out and back, so to find the distance of the
+// object we take half of the distance travelled.
+return microseconds / 29 / 2;
+}
